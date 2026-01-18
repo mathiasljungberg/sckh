e2c67b967ac7898b2637a087d4bae01b9f3c1bbb
mathiasljungberg <mathias.ljungberg@gmail.com>
2026-01-18 15:24:40 -0300

    started classial 2d dynamics in python

---
+++ src/python_scripts/dynamics_2d.py
@@ -0,0 +1,127 @@
+import numpy as np
+from numpy.polynomial import Polynomial
+import matplotlib.pyplot as plt
+from scipy.interpolate import RectBivariateSpline
+
+
+def poly_on_grid(x, x0=0, coeffs=[1,2,3]):
+    # polynomials and derivatives
+    p = Polynomial(coeffs)
+    dp = p.deriv()
+
+    # polynmial and derivative on grid
+    return p(x-x0), dp(x-x0)
+
+
+# product potental energy surface, 2d
+def product_poly_2d(x,y, x0=0,y0=0, coeffs_x=[1,2,3], coeffs_y=[1,2,3]):
+
+    # polynomials and derivatives
+    fx, dfx = poly_on_grid(x, x0=x0, coeffs=coeffs_x)
+    fy, dfy = poly_on_grid(y, x0=y0, coeffs=coeffs_y)
+
+    return {'f': fx[:, None] @ fy[None, :], 
+            'df_dx': dfx[:, None] @ fy[None, :],
+            'df_dy': fx[:, None] @ dfy[None, :]}
+
+if __name__ == "__main__":
+    x= np.linspace(-1,1, 5)
+    y= np.linspace(-1,1, 5)
+
+    ret = product_poly_2d(x,y, x0=0,y0=0, 
+                          coeffs_x=[1,2,3,4], 
+                          coeffs_y=[1,2,3])
+    
+    # spline interpolation on 2d grid
+    Z = ret.get('f')
+    spl = RectBivariateSpline(x, y, Z, kx=3, ky=3)
+
+
+    # make sure spline and derivatives come out right at grid points
+    f_spl = spl(x,y, dx=0, dy=0)
+    assert(np.max(f_spl - ret.get('f')) < 1e-12)
+    f_spl = spl(x,y, dx=1, dy=0)
+    assert(np.max(f_spl - ret.get('df_dx')) < 1e-12)
+    f_spl = spl(x,y, dx=0, dy=1)
+    assert(np.max(f_spl - ret.get('df_dy')) < 1e-12)
+
+
+    X, Y = np.meshgrid(x, y, indexing="ij")
+
+    # --- Plot ---
+    fig = plt.figure(figsize=(12, 4))
+
+    # 3D surface
+    
+    Z = ret.get('f')
+    ax1  = fig.add_subplot(1, 3, 1, projection="3d")
+    ax1.plot_surface(X, Y, Z, cmap="viridis")
+    ax1.set_title("Z = f(x) · g(y)")
+    ax1.set_xlabel("x")
+    ax1.set_ylabel("y")
+
+    Z = ret.get('df_dx')
+    ax2  = fig.add_subplot(1, 3, 2, projection="3d")
+    ax2.plot_surface(X, Y, Z, cmap="viridis")
+    ax2.set_title("Z = df(x)/dx · g(y)")
+    ax2.set_xlabel("x")
+    ax2.set_ylabel("y")
+
+    Z = ret.get('df_dy')
+    ax2  = fig.add_subplot(1, 3, 3, projection="3d")
+    ax2.plot_surface(X, Y, Z, cmap="viridis")
+    ax2.set_title("Z = f(x) · dg(y)/dy")
+    ax2.set_xlabel("x")
+    ax2.set_ylabel("y")
+
+
+    # plot the thing on finer grid
+    xf= np.linspace(x.min(),x.max(), 200)
+    yf= np.linspace(y.min(),y.max(), 200)
+
+
+    retf = product_poly_2d(xf,yf, x0=0,y0=0, 
+                          coeffs_x=[1,2,3,4], 
+                          coeffs_y=[1,2,3])
+
+    # make sure spline and derivatives come out right at finer grid points
+    f_spl = spl(xf,yf, dx=0, dy=0)
+    assert(np.max(f_spl - retf.get('f')) < 1e-12)
+    f_spl = spl(xf,yf, dx=1, dy=0)
+    assert(np.max(f_spl - retf.get('df_dx')) < 1e-12)
+    f_spl = spl(xf,yf, dx=0, dy=1)
+    assert(np.max(f_spl - retf.get('df_dy')) < 1e-12)
+
+    ssasda
+
+    Xf, Yf = np.meshgrid(xf, yf, indexing="ij")
+
+    fig = plt.figure(figsize=(12, 4))
+
+    # 3D surface
+    Zf   = spl(xf, yf) 
+    ax1  = fig.add_subplot(1, 3, 1, projection="3d")
+    ax1.plot_surface(Xf, Yf, Zf, cmap="viridis")
+    ax1.set_title("Z = f(x) · g(y)")
+    ax1.set_xlabel("x")
+    ax1.set_ylabel("y")
+
+    Zf = spl(xf, yf, dx=1, dy=0)    
+    ax2  = fig.add_subplot(1, 3, 2, projection="3d")
+    ax2.plot_surface(Xf, Yf, Zf, cmap="viridis")
+    ax2.set_title("Z = df(x)/dx · g(y)")
+    ax2.set_xlabel("x")
+    ax2.set_ylabel("y")
+
+    Zf = spl(xf, yf, dx=0, dy=1) 
+    ax2  = fig.add_subplot(1, 3, 3, projection="3d")
+    ax2.plot_surface(Xf, Yf, Zf, cmap="viridis")
+    ax2.set_title("Z = f(x) · dg(y)/dy")
+    ax2.set_xlabel("x")
+    ax2.set_ylabel("y")
+
+
+    #axs.plot(x, fx, '--o')
+    #axs.plot(x, dfx,'--o')
+
+    plt.show()
